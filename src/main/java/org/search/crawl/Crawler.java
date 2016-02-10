48caab974d627fa4e77003b56933c52874dcf687
Johnny Jurado <jwj9627@gmail.com>
2016-02-10 15:01:01 -0800

    Add my code to existing code.

--- src/main/java/org/search/crawl/Crawler.java
+++ src/main/java/org/search/crawl/Crawler.java
@@ -2,6 +2,7 @@ package org.search.crawl;
 
 import java.io.IOException;
 
+import org.apache.commons.cli.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -10,10 +11,32 @@ public class Crawler {
 
     public static void main(String[] args) throws IOException
     {
-    	System.out.println("Crawler started");
-    	
-    	
-    	String url = "http://en.wikipedia.org/";
+        int depth;
+        String url;
+        boolean extraction;
+
+        // Get Arguments
+        Options options = setOptions();
+        // Parse Arguments
+        CommandLine cmd = parseArg(options, args);
+
+        // Check if Depth and Url were passed as arguments
+        // Exit if arguments are not passed
+        if (!cmd.hasOption("d") || !cmd.hasOption("u")) {
+            System.out.println("Arguments needed Url and Depth");
+            return;
+        }
+
+        // Set Values from the parameters
+        depth = Integer.parseInt(cmd.getOptionValue("d"));
+        url = cmd.getOptionValue("u");
+        extraction = cmd.hasOption("e");
+
+        System.out.printf("Depth:%d, Url:%s, extraction:%b \n", depth, url, extraction);
+        /*----------------------------------------------------------------------------------------------------------*/
+
+        System.out.println("Crawler started");
+    	//String url = "http://en.wikipedia.org/";
     	downloadPage(url);
     }
 
@@ -23,5 +46,29 @@ public class Crawler {
     	Elements newsHeadlines = doc.select("#mp-itn b a");
     	
     	System.out.println(newsHeadlines.toString());
+    }
+
+    public static Options setOptions() {
+        Options options = new Options();
+
+        options.addOption("d", true, "Depth");
+        options.addOption("u", true, "Url");
+        options.addOption("e", false, "extraction mode");
+
+        return options;
+    }
+
+    public static CommandLine parseArg(Options options, String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return cmd;
     }
 }
