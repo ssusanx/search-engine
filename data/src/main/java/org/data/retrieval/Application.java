d6f4985797847d120bdff7df34b05528d72ffd28
ssusanx <ssusanx0507@gmail.com>
2016-02-13 00:54:09 -0800

    added data dump

--- data/src/main/java/org/data/retrieval/Application.java
+++ data/src/main/java/org/data/retrieval/Application.java
@@ -1,13 +1,34 @@
 package org.data.retrieval;
 
-import java.io.IOException;
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 
 public class Application {
 
     public static void main(String[] args)
     {
+    	MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+    	MongoCollection<Document> collection = null;
+    	MongoDatabase database = null;
+    	
+		database = mongoClient.getDatabase("local");
+		collection = database.getCollection("test");
 
         System.out.println("data dump started");
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+        
+        mongoClient.close();
     }
     
 }
