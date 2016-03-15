f56dd0603cd0e238e746c889d455dcb45f13f6eb
ssusanx <ssusanx0507@gmail.com>
2016-03-15 13:12:15 -0700

    added weighting scores

--- query/src/main/java/org/search/query/service/SearchResult.java
+++ query/src/main/java/org/search/query/service/SearchResult.java
@@ -1,9 +1,14 @@
 package org.search.query.service;
 
-public class SearchResult {
+import java.util.Comparator;
+
+public class SearchResult implements Comparable<SearchResult>{
 	
 	private String title;
 	private String link;
+	private double tfidf;
+	private double linkAnalysis;
+	private double score;
 	
 	public String getTitle() {
 		return title;
@@ -16,6 +21,34 @@ public class SearchResult {
 	}
 	public void setLink(String link) {
 		this.link = link;
+	}
+	public double getTfidf() {
+		return tfidf;
+	}
+	public void setTfidf(double tfidf) {
+		this.tfidf = tfidf;
+	}
+	public double getLinkAnalysis() {
+		return linkAnalysis;
+	}
+	public void setLinkAnalysis(double linkAnalysis) {
+		this.linkAnalysis = linkAnalysis;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	@Override
+	public int compareTo(SearchResult o1) {
+		
+		if (this.score < o1.score)
+			return 1;
+		else if (this.score > o1.score)
+			return -1;
+		else
+			return 0;
 	}
 
 }
